f49711d5c955ce640ffbe2fb4b8655513cab0e63
jilcimar <jilcimar.fernandes0267@gmail.com>
2018-05-16 19:33:35 -0300

    Fazendo o primeiro teste de histórico

--- app/Administracao.php
+++ app/Administracao.php
@@ -3,12 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Venturecraft\Revisionable\Revisionable;
 use \Venturecraft\Revisionable\RevisionableTrait;
 
-class Administracao extends Model
+class Administracao extends Revisionable
 {
     protected $revisionEnabled = true;
-    protected $historyLimit = 500; //Interromper as reviões depois de 500 alterações
+   // protected $historyLimit = 500; //Interromper as reviões depois de 500 alterações
+
+    //Especificando quais campos deseja não rastrear
+    protected $dontKeepRevisionOf = array(
+        'fim'
+    );
 
     protected $table = 'administracao';
     protected $fillable = [
--- resources/views/admin/list.blade.php
+++ resources/views/admin/list.blade.php
@@ -14,6 +14,20 @@
                     </div>
                 </div>
             </div>
+
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Histórico modificação</div>
+
+                    <div class="panel-body">
+                        @foreach($admins as $admin)
+                            @foreach($admin->revisionHistory as $history)
+                                <li>{{ $history->userResponsible()->first_name }} Alterou {{ $history->fieldName() }} de: {{ $history->oldValue() }} para: {{ $history->newValue() }}</li>
+                            @endforeach
+                        @endforeach
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
